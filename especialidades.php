e69acddd44dfad5302c0334b69e30b00b6c967c5
ravilaj <134107400+ravilaj@users.noreply.github.com>
2023-05-21 23:24:09 +0200

    Update especialidades.php

--- especialidades.php
+++ especialidades.php
@@ -4,42 +4,42 @@
 ?>
 
 
-<article style="height: 550px; text-align: center;">
+<article class="especia">
 
     <div class="prin" style="margin: 0 auto; width: 100%; text-align: center;">
         <a href="especialidad.php?esp=dermatologia" style="display: inline-block;">
-        <div style="float: left; margin: 10px; padding: 10px;">
-            <img src="img/dermatologia.webp" style="width: 400px; height: 175px; display: block;">
+        <div class="especialidad" style="float: left; margin: 10px; padding: 10px;">
+            <img src="img/dermatologia.webp" >
             <h4>Dermatología</h4>
         </div>
 		</a>
         <a href="especialidad.php?esp=cardiologia" style="display: inline-block;">
-        <div style="float: left; margin: 10px; padding: 10px;">
-            <img src="img/cardiologia.webp" style="width: 400px; height: 175px; display: block;">
+        <div class="especialidad" style="float: left; margin: 10px; padding: 10px;">
+            <img src="img/cardiologia.webp">
             <h4>Cardiología</h4>
         </div>
 		</a>
 		<a href="especialidad.php?esp=otorrinolaringologia" style="display: inline-block;">
-        <div style="float: left; margin: 10px; padding: 10px;">
-            <img src="img/otorrino.webp" style="width: 400px; height: 175px; display: block;">
+        <div class="especialidad" style="float: left; margin: 10px; padding: 10px;">
+            <img src="img/otorrino.webp">
             <h4>Otorrinolaringología</h4>
         </div>
 		</a>
 		<a href="especialidad.php?esp=alergologia" style="display: inline-block;">
         <div style="float: left; margin: 10px; padding: 10px;">
-            <img src="img/alergias.webp" style="width: 400px; height: 175px; display: block;">
+            <img class="especialidad" src="img/alergias.webp">
             <h4>Alergología</h4>
         </div>
 		</a>
 		<a href="especialidad.php?esp=nutricio" style="display: inline-block;">
         <div style="float: left; margin: 10px; padding: 10px;">
-            <img src="img/nutricion.webp" style="width: 400px; height: 175px; display: block;">
+            <img class="especialidad" src="img/nutricion.webp">
             <h4>Nutrición</h4>
         </div>
 		</a>
 		<a href="especialidad.php?esp=pediatra" style="display: inline-block;">
         <div style="float: left; margin: 10px; padding: 10px;">
-            <img src="img/pediatria.webp" style="width: 400px; height: 175px; display: block;">
+            <img class="especialidad" src="img/pediatria.webp">
             <h4>Pediatría</h4>
         </div>
 		</a>
